55ce1bf4b6fbafc6bd5ce8aaa60045ede8470872
Surya Varma <d.s.r.suryavarma@gmail.com>
2025-04-03 13:44:56 +0530

    Update dynamo_app.py

--- dynamo_app.py
+++ dynamo_app.py
@@ -127,4 +127,5 @@ def donate_blood(request_id):
     return redirect(url_for('dashboard'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
